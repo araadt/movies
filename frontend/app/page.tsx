12e225d6db7f639d3a2ca261a9b80ddf439c5525
Andy van der Raadt <andy@vanderraadt.codes>
2025-05-02 13:24:51 -0600

    feat(ui): scaffold placeholder FilmMetadata component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/hero";
-
+import FilmMetadata from "@/components/films/metadata";
 export default async function Home() {
   return (
     <>
@@ -8,6 +8,7 @@ export default async function Home() {
         <h2 className="font-medium text-lg mb-4">Lorem ipsum steps</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </main>
+      <FilmMetadata id={123} />
     </>
   );
 }
