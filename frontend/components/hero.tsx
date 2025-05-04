21ddf7b52f15c47a7edd257b4e4635271b2df385
Andy van der Raadt <andy@vanderraadt.codes>
2025-05-03 20:45:11 -0600

    refactor(hero): update hero header with something non-ipsum (but still crappy)

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -18,7 +18,7 @@ export default function Header() {
       </div> */}
       <h1 className="sr-only" data-testid="hero-title" data-text="Andy's Films" data-todo="ipsum">Andy's Films</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        Discover and browse. There's something for everyone.
       </p>
       {/* <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" /> */}
     </div>
