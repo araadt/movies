9a0fec82b4ae32be4c10a10a7f89ca1f71904b99
Andy van der Raadt <andy@vanderraadt.codes>
2025-05-05 09:16:16 -0600

    refactor(ui): update hero text

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -2,25 +2,11 @@
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      {/* <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div> */}
+    <div className="flex flex-col gap-4 items-center justify-center min-h-[20svh]">
       <h1 className="sr-only" data-testid="hero-title" data-text="Andy's Films" data-todo="ipsum">Andy's Films</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Discover and browse. There's something for everyone.
+      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center text-pretty">
+        Browse, search, and discover. Movies and TV for everyone.
       </p>
-      {/* <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" /> */}
     </div>
   );
 }
